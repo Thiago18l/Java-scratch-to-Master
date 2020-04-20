79ea703d2d5822802ad12ef58aa9c8b93e6802f1
Thiago <thiago.lopes.dev@gmail.com>
2020-04-20 13:57:34 -0300

    entendendo a stacktrace

--- Exceptions/src/Module/TryCatch.java
+++ Exceptions/src/Module/TryCatch.java
@@ -5,18 +5,30 @@ import java.util.Scanner;
 
 public class TryCatch {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-            try{
-                String[] vetor = input.nextLine().split(" ");
-                int position = input.nextInt();
-                System.out.println(vetor[position]);
-            }catch (ArrayIndexOutOfBoundsException e){
-                System.out.println("Invalid Position");
-            }catch (InputMismatchException e){
-                System.out.println("Input error");
-            }
-
+            method1();
             System.out.println("End of Program");
 
+    }
+    public static void method1() {
+        System.out.println("METHOD 1 START");
+        method2();
+        System.out.println("METHOD 1 END");
+    }
+
+    public static void method2() {
+
+        System.out.println("method 2 *** start");
+        Scanner input = new Scanner(System.in);
+        try{
+            String[] vetor = input.nextLine().split(" ");
+            int position = input.nextInt();
+            System.out.println(vetor[position]);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Invalid Position");
+        }catch (InputMismatchException e){
+            System.out.println("Input error");
+        }
+        input.close();
+        System.out.println("method 2 *** end");
     }
 }
