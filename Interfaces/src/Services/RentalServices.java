17ae86d2b419519473fbe3778447c2da53fd27c6
Thiago <thiago.lopes.dev@gmail.com>
2020-04-24 17:05:24 -0300

    feat: add the calculator of tax

--- Interfaces/src/Services/RentalServices.java
+++ Interfaces/src/Services/RentalServices.java
@@ -1,6 +1,7 @@
 package Services;
 
 import Entities.CarRental;
+import Entities.Invoice;
 
 public class RentalServices {
     private Double pricePerDay;
@@ -38,6 +39,17 @@ public class RentalServices {
         this.taxService = taxService;
     }
     public void processInvoice(CarRental carRental){
+        long t1 = carRental.getStart().getTime();
+        long t2 = carRental.getFinish().getTime();
+        double hours = (double)(t2 - t1) / 1000 / 60 / 60;
+        double basicPayment;
+        if (hours <= 12.0){
+            basicPayment = Math.ceil(hours)* pricePerHour;
+        }else {
+            basicPayment = Math.ceil(hours / 24) * pricePerDay;
+        }
+        double tax = taxService.tax(basicPayment);
+        carRental.setInvoice(new Invoice(basicPayment, tax));
 
     }
 }
