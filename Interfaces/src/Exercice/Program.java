3a1fb554e09b6934a68cfcf68a74d1610e2fdb3c
Thiago <thiago.lopes.dev@gmail.com>
2020-04-28 09:11:53 -0300

    chore: add a class program to execute

--- Interfaces/src/Exercice/Program.java
+++ Interfaces/src/Exercice/Program.java
@@ -1,4 +1,48 @@
 package Exercice;
 
+import Exercice.Entities.Contract;
+import Exercice.Entities.Installment;
+import Exercice.Service.ContractService;
+import Exercice.Service.PayPalService;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    try{
+        System.out.println("Enter contract data");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+        System.out.print("Date (dd/MM/yyyy): ");
+        Date date = sdf.parse(sc.next());
+        System.out.print("Contract value: ");
+        double totalValue = sc.nextDouble();
+
+        Contract contract = new Contract(number, date, totalValue);
+
+        System.out.print("Enter number of installments: ");
+        int n = sc.nextInt();
+
+        ContractService contractService = new ContractService(new PayPalService());
+
+        contractService.processContract(contract, n);
+
+        System.out.println("Installments:");
+        for (Installment x : contract.getInstallments()) {
+            System.out.println(x);
+        }
+    } catch (ParseException e){
+        System.out.println(e.getMessage());
+    }
+
+
+        sc.close();
+    }
 }
