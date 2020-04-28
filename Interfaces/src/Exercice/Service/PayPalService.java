a293baf12bb95962109228ffb58a87e084eae44b
Thiago <thiago.lopes.dev@gmail.com>
2020-04-28 09:11:25 -0300

    chore: add PayPalService class

--- Interfaces/src/Exercice/Service/PayPalService.java
+++ Interfaces/src/Exercice/Service/PayPalService.java
@@ -1,4 +1,15 @@
 package Exercice.Service;
 
-public class PayPalService {
+public class PayPalService implements OnlinePaymentService {
+    private static final double FEE_PERCENTAGE = 0.02;
+    private static final double MONTHLY_INTEREST = 0.01;
+    @Override
+    public double paymentFee(double amount) {
+        return amount * FEE_PERCENTAGE;
+    }
+
+    @Override
+    public double interest(double amount, int months) {
+        return amount * MONTHLY_INTEREST * months;
+    }
 }
