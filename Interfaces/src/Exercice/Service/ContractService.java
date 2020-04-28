c0c63f36d6a26009152eee6d9c74e66030aec959
Thiago <thiago.lopes.dev@gmail.com>
2020-04-28 09:10:14 -0300

    chore: add ContractService class

--- Interfaces/src/Exercice/Service/ContractService.java
+++ Interfaces/src/Exercice/Service/ContractService.java
@@ -1,4 +1,31 @@
 package Exercice.Service;
 
+import Exercice.Entities.Contract;
+import Exercice.Entities.Installment;
+
+import java.util.Calendar;
+import java.util.Date;
+
 public class ContractService {
+
+    private OnlinePaymentService onlinePaymentService;
+    public ContractService(OnlinePaymentService onlinePaymentService){
+        this.onlinePaymentService = onlinePaymentService;
+    }
+
+    public void processContract (Contract contract, int months){
+        double basicQuota = contract.getTotalValue() / months;
+            for (int i = 1; i <= months; i++) {
+                Date date = addMonths(contract.getDate(), i);
+                double updateQuota = basicQuota + onlinePaymentService.interest(basicQuota, i);
+                double fullQuota = updateQuota + onlinePaymentService.paymentFee(updateQuota);
+                contract.addInstallment(new Installment(date, fullQuota));
+            }
+    }
+    private Date addMonths(Date date, int n) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.MONTH, n);
+        return calendar.getTime();
+    }
 }
