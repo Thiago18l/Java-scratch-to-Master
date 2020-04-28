be396c71873e54854f1f0ff58ab87a12817095fb
Thiago <thiago.lopes.dev@gmail.com>
2020-04-28 17:44:50 -0300

    chore: add a class to run the program

--- Interfaces/src/Exercice2/Program.java
+++ Interfaces/src/Exercice2/Program.java
@@ -1,4 +1,25 @@
 package Exercice2;
 
+import Exercice2.Services.BrazilInterestService;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+            System.out.print("Amount:");
+            double amount = input.nextDouble();
+            System.out.print("Months: ");
+            int months = input.nextInt();
+
+        BrazilInterestService is = new BrazilInterestService(2.0);
+        double payment = is.payment(amount, months);
+        System.out.println("Payment after " + months + " months:" );
+            System.out.println(String.format("%.2f", payment));
+
+            input.close();
+    }
 }
