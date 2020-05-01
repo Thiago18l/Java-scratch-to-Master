bcc7e2ef18056ad046f3959421a7c186a666f719
Thiago <thiago.lopes.dev@gmail.com>
2020-05-01 14:59:04 -0300

    refactor: add new methods equals and hashCode

--- Generics, Set, Map/src/Map/Entities/Product.java
+++ Generics, Set, Map/src/Map/Entities/Product.java
@@ -1,5 +1,7 @@
 package Map.Entities;
 
+import java.util.Objects;
+
 public class Product {
     private String name;
     private Double price;
@@ -23,5 +25,19 @@ public class Product {
 
     public void setPrice(Double price) {
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return Objects.equals(getName(), product.getName()) &&
+                Objects.equals(getPrice(), product.getPrice());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getPrice());
     }
 }
