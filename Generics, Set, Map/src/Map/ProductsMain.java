fb59cf05a69ea043034f58ddc2bc96568836972d
Thiago <thiago.lopes.dev@gmail.com>
2020-05-01 14:57:28 -0300

    feat: add products in the main class

--- Generics, Set, Map/src/Map/ProductsMain.java
+++ Generics, Set, Map/src/Map/ProductsMain.java
@@ -1,7 +1,23 @@
 package Map;
 
+import Map.Entities.Product;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProductsMain {
     public static void main(String[] args) {
+        Map<Product, Double> stock = new HashMap<>();
+        Product p1 = new Product("Tv", 900.0);
+        Product p2 = new Product("Iphone", 399.0);
+        Product p3 = new Product("Notebook", 1200.0);
+
+            stock.put(p1, 1000.0);
+            stock.put(p2, 500.0);
+            stock.put(p3, 250.0);
+
+            Product ps = new Product("Tv", 900.0);
+                System.out.println("Contains 'ps' key: " + stock.containsKey(ps));
 
     }
 }
