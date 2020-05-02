5a91ff62c3d3eef072ef09fb92abf9022e434c90
Thiago <thiago.lopes.dev@gmail.com>
2020-05-02 16:44:37 -0300

    implements the interface predicate to test and return they products cheaper than 100.0

--- Lambdas/src/Predicate/Utils/ProductPredicate.java
+++ Lambdas/src/Predicate/Utils/ProductPredicate.java
@@ -1,4 +1,12 @@
 package Predicate.Utils;
 
-public class ProductPredicate {
+import Predicate.Entities.Product;
+
+import java.util.function.Predicate;
+
+public class ProductPredicate implements Predicate<Product> {
+    @Override
+    public boolean test(Product product) {
+        return product.getPrice() >= 100.0;
+    }
 }
