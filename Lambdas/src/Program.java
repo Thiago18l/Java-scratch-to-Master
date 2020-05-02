99c4e9236ae2a2322458a9aeba03735c68bb2708
Thiago <thiago.lopes.dev@gmail.com>
2020-05-02 13:19:06 -0300

    refactor: implements method to use Lambdas expressions (Arrow functions)

--- Lambdas/src/Program.java
+++ Lambdas/src/Program.java
@@ -2,6 +2,7 @@ import Entities.MyComparator;
 import Entities.Product;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Program {
@@ -10,8 +11,11 @@ public class Program {
 
         products.add(new Product("Tv", 900.0));
         products.add(new Product("Notebook", 1200.00));
+        Comparator<Product> comp = (p1, p2) -> {
+          return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+        };
 
-        products.sort(new MyComparator());
+        products.sort(comp);
         for (Product p : products) {
             System.out.println(p);
         }
