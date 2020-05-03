0663f2bea4fe87e4402600d68d68b2d27a34d16c
Thiago <thiago.lopes.dev@gmail.com>
2020-05-03 12:01:08 -0300

    refactor: using the method reference on forEach

--- Lambdas/src/Consumer/Program.java
+++ Lambdas/src/Consumer/Program.java
@@ -5,15 +5,17 @@ import Consumer.Util.PriceUpdate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         List<Product> list = new ArrayList<>();
         list.add(new Product("Tv", 900.0));
         list.add(new Product("Notebook", 1200.0));
         list.add(new Product("Mouse", 50.0));
         list.add(new Product("Tesoura", 25.0));
-            list.forEach(new PriceUpdate());
+            list.forEach(Product::staticPriceUpdate);
             list.forEach(System.out::println);
     }
 }
