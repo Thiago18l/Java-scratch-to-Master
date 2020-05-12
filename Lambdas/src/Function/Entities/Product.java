615d7164d0ba21d4f80e77d6b78de93c7f7eb011
Thiago <thiago.lopes.dev@gmail.com>
2020-05-12 01:25:35 -0300

    Uppercase Product with Function

--- Lambdas/src/Function/Entities/Product.java
+++ Lambdas/src/Function/Entities/Product.java
@@ -1,4 +1,39 @@
 package Function.Entities;
 
 public class Product {
+    private String name;
+    private Double price;
+
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    public String UpperCaseName(){
+        return name.toUpperCase();
+    }
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+
 }
